0620db05664164eac7bfd642da8d64535b57f360
LILIAN MARIA SILVA PINTO <2024303320@ifam>
2026-03-23 10:41:43 -0400

    seila

--- super.js
+++ super.js
@@ -8,9 +8,12 @@ let preco = Number(inPreco.value)
 let total = preco * 3
 let desconto = preco * 0.5
 let valorFinal = total - desconto
+let precoUltimo = preco - desconto
 
 outProduto.textContent = "Promoção de " + produto
 outPromocao.textContent = "Leve 3 por apenas R$ " + valorFinal
+outUltimo.textContent = "O terceiro produto ficou por R$" + precoUltimo
 }
+
 let mostrar = document.getElementById("mostrar") 
 mostrar.addEventListener("click", calcularPromocao)
